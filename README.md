98351aee10f22b20603d4ccd9908e82ca3a34995
db0company <db0company@gmail.com>
2012-08-06 15:38:50 +0200

    Script to generate files, compile and launch the server to test ocsigen examples

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
 Ocsigen-Quick-Howto
 ===================
 
-Ocsigen (OCaml Web Server + Framework) sample code to illustrate the "Quick HowTo".
+Ocsigen is a poweful web server and framework in OCaml.
+The documentation is providing these sample codes in the "Quick Howto" section.
+
+
+## Test it!
+
+To test these examples, type:
+
+    /make.sh example_directory
+
+This script will generate a Makefile and a configuration file then compile
+and launch an ocsigen server.
+
+
+## Available examples
+
+* __Skeletton__
+  Documentation URL coming soon...
+
+
+## Copyright/License
+
+
+     Copyright 2012 Barbara Lepage
+  
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+  
+         http://www.apache.org/licenses/LICENSE-2.0
+  
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+
+
+### Author
+
+* Made by __db0__
+* Website: http://db0.fr/
+* Contact: db0company@gmail.com
+
+
+### Up to date
+
+Latest version of this project is on GitHub:
+https://github.com/db0company/Ocsigen-Quick-Howto/
---
+++ make.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+## ########################################################################## ##
+## Project: Ocsigen Quick Howto                                               ##
+## Description: Script to generate Makefile and configuration file then       ##
+##              compile and launch to test the Quick Howto examples           ##
+## Author: db0 (db0company@gmail.com, http://db0.fr/)                         ##
+## Latest Version is on GitHub: http://goo.gl/sfvvq                           ##
+## ########################################################################## ##
+
+generated_files="files"
+
+function	usage() {
+    echo "usage: $0 example_directory" >&2
+    exit 1
+}
+
+function	init() {
+    generated_files=$1
+    project_name=$2
+    mkdir -p $generated_files && \
+	mkdir -p tmp && \
+	cp .make/Makefile $generated_files/ && \
+	cp .make/example.conf $generated_files/ && \
+	cp $project_name/*.eliom $generated_files/ && \
+	return 0
+    return 1
+}
+
+function	generate_makefile() {
+    generated_files=$1
+    file=$2
+    files=`\ls -1 $generated_files/ \
+	| \grep '.eliom$' \
+	| xargs \
+	| sed 's# #\$SPACE\$#' \
+	| sed 's#\/#\\\/#g'`
+    echo "Generating Makefile... " && \
+	sed -i".bak" 's#\$FILES\$#'$files'#' $file && \
+	sed -i".bak" 's/\$SPACE\$/ /g' $file && \
+	echo "Done." && \
+	return 0
+    return 1
+}
+
+function	generate_conf_file() {
+    generated_files=$1
+    file=$2
+    pwd=`pwd | sed 's#\/#\\\/#g'`
+    files=`\ls -1 $generated_files/ \
+	| \grep '.eliom$' \
+	| sed 's#\.eliom$#\.cmo#' \
+	| sed 's#^#<eliom\$SPACE\$module=\\\"_server\\\/#' \
+	| sed 's#$#\\\"\$SPACE\$\\\/>#' \
+	| sed 's#\/#\\\/#g' \
+	| xargs \
+	| sed 's#\ ##'`
+    echo "Generating Configuration file... "
+    echo -n "Port number? " && \
+	read port && \
+	sed -i".bak" 's/\$PORT\$/'$port'/' $file && \
+	sed -i".bak" 's/\$PWD\$/'$pwd'/' $file && \
+	sed -i".bak" 's/\$FILES\$/'$files'/' $file && \
+	sed -i".bak" 's/\$SPACE\$/ /g' $file && \
+	echo "Done." && \
+	return 0
+    return 1
+}
+
+function	compile() {
+    generated_files=$1
+    echo "Compile..." && \
+	cd $generated_files && \
+	make && \
+	echo "Done." && \
+	return 0
+    return 1
+}
+
+function	launch() {
+    echo -n "Launch server with generated files (Y/n)? "
+    read answer
+    if [ -z "$answer" ]||[ $answer == "y" ]
+    then
+	type ocsigenserver > /dev/null
+	if [ $? -ne 0 ]
+	then
+	    echo >&2 "OcsigenServer is not installed!"
+	else
+	    trap "echo 'Server killed.'" SIGINT SIGTERM
+	    echo "Launch server..." && \
+		sh -c "sh -c 'ocsigenserver -c example.conf'" && \
+		echo "Done."
+	fi
+    fi
+}
+
+function	clean() {
+    generated_files=$1
+    rm -f *.bak
+    cd - > /dev/null
+    rm -rf tmp
+    echo -n "Remove generated files (Y/n)? "
+    read answer
+    if [ -z "$answer" ]||[ $answer == "y" ]
+    then
+	rm -rf $generated_files
+	echo "Done"
+    else
+	echo "Generated files are in \"$generated_files\" folder."
+	ls $generated_files
+    fi
+}
+
+if [ $# -ne 1 ]||[ ! -d $1 ]
+then usage
+fi
+
+init $generated_files $1 && \
+    generate_makefile $generated_files "$generated_files/Makefile" && \
+    generate_conf_file $generated_files "$generated_files/example.conf" && \
+    compile $generated_files && \
+    launch
+clean $generated_files
+
+
