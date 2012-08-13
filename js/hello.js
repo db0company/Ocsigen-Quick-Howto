e21c11033160e2819d67e42ad7c9382f45af01d6
db0company <db0company@gmail.com>
2012-08-13 11:50:17 +0200

    Js example, javascript file

---
+++ js/hello.js
@@ -0,0 +1,10 @@
+/* ************************************************************************** */
+/* Project: Ocsigen Quick Howto : Javascript Inclusion                        */
+/* Description: Example of a page including external javascript script        */
+/* Author: db0 (db0company@gmail.com, http://db0.fr/)                         */
+/* Latest Version is on GitHub: http://goo.gl/sfvvq                           */
+/* ************************************************************************** */
+
+function hello_function() {
+    alert("Hello!");
+}
